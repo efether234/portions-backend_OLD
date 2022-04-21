243f480b3312a4af2c0df179367103f4738a7fae
Evan Fetherolf <efether234@gmail.com>
2022-04-20 19:45:48 -0700

    Add CORS.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
+const express = require('express')
 const mongoose = require('mongoose')
 const date = require('date-and-time')
-const express = require('express')
+const cors = require('cors')
 
 mongoose.connect('mongodb://localhost:27017/portions')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 defaultDate = new Date()
 
